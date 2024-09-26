aaf21fe87f1e21d8db55478e9a22c1218ab5c19f
César Danilo <87550603+CesarDanilo@users.noreply.github.com>
2024-09-25 23:48:18 -0300

    UPDATE FILES

---
+++ rockstore/src/components/ReviewsList.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+export default function ReviewsList() {
+  return (
+    <div className="page-inner-content">
+        <div className="reviews">
+            <div className="review">
+                <p>"</p>
+                <p>
+                Estou extremamente satisfeita com minha compra! O atendimento foi excelente e a entrega super rápida.
+                Os produtos são de alta qualidade e exatamente como descritos no site. 
+                Com certeza voltarei a comprar aqui!
+                </p>
+                <p className="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                <p>Maria Oliveira</p>
+            </div>
+            <div className="review">
+                <p>"</p>
+                <p>
+                A experiência de compra foi incrível! O site é fácil de navegar e encontrei tudo o que precisava.
+                Além disso, o suporte ao cliente foi muito atencioso e resolveu todas as minhas dúvidas rapidamente.
+                Recomendo a todos!
+                </p>
+                <p className="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                <p>João Silva</p>
+            </div>
+            <div className="review">
+                <p>"</p>
+                <p>
+                Comprei um notebook e alguns acessórios e fiquei impressionado com a eficiência do serviço.
+                Os preços são competitivos e a variedade de produtos é excelente.
+                Recebi tudo em perfeito estado e dentro do prazo.
+                Parabéns pela ótima loja!
+                </p>
+                <p className="rate">&#9733;&#9733;&#9733;&#9733;&#9733;</p>
+                <p>Ana Souza</p>
+            </div>
+        </div>
+    </div>
+  )
+}
